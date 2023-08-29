e1b2d64f953da70834c88c64944bc7d04dd14ccc
Stephen Fung <sfung1@bcchr.ca>
2023-08-29 16:57:03 -0700

    Adding Appbar to Submitterlayout

--- web/src/layouts/SubmitterLayout/SubmitterLayout.jsx
+++ web/src/layouts/SubmitterLayout/SubmitterLayout.jsx
@@ -1,8 +1,22 @@
+import AdbIcon from '@mui/icons-material/Adb'
+import MenuIcon from '@mui/icons-material/Menu'
+import AppBar from '@mui/material/AppBar'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Toolbar from '@mui/material/Toolbar'
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+
 import { Link, routes } from '@redwoodjs/router'
 const SubmitterLayout = ({ children }) => {
   return (
     <>
-      <header>
+      {/*       <header>
         <h1>EPUP</h1>
         <nav>
           <ul>
@@ -14,7 +28,34 @@ const SubmitterLayout = ({ children }) => {
             </li>
           </ul>
         </nav>
+      </header> */}
+      <header>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                LOGO
+              </Typography>
+            </Toolbar>
+          </Container>
+        </AppBar>
       </header>
+
       <main>{children}</main>
     </>
   )
