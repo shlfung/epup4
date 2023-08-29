d7979b546916a1895e1e5ef1d27f905c2c3ddbf1
Stephen Fung <sfung1@bcchr.ca>
2023-08-29 15:23:13 -0700

    Setup MUI

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,6 +4,11 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import './scaffold.css'
 import './index.css'
 
