b33e4da7042c7f59b13d50a09aeee11fffe927af
Stephen Fung <sfung1@bcchr.ca>
2023-08-30 14:26:32 -0700

    display project using card

--- web/src/components/ProposalsCell/ProposalsCell.jsx
+++ web/src/components/ProposalsCell/ProposalsCell.jsx
@@ -1,3 +1,11 @@
+import { Divider } from '@mui/material'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+
 export const QUERY = gql`
   query ProposalsQuery {
     proposals: projects {
@@ -19,10 +27,20 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ proposals }) => {
   return (
-    <ul>
-      {proposals.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
+    <>
+      {proposals.map((proposal) => (
+        <Card key={proposal.id} sx={{ minWidth: 275 }} variant="outlined">
+          <CardContent>
+            <Typography sx={{ fontSize: 22 }} color="text.primary" gutterBottom>
+              {proposal.title}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+          <br></br>
+        </Card>
+      ))}
+    </>
   )
 }
